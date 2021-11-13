25a61581593b94ea02ab510f55fc56ba6faf1402
Sergei0505 <publicmail@tutanota.de>
2021-11-14 01:42:40 +0500

    Draw 2 point on the screen

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,24 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            const int widthConsole = 40;
+            const int heightConsole = 30;
+            Console.SetWindowSize(widthConsole, heightConsole);
+            Console.SetBufferSize(widthConsole, heightConsole);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Draw(x1, y1, c1);
+
+            Console.ReadLine();
+        }
+        
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
